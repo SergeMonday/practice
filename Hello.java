9a37cfc44ccbdbecb20a5ff41cab3c13496ae4fb
SergeMonday <roooten@mail.ru>
2019-04-14 21:07:28 +0300

    first commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+	
+	static int a = 6;
+
+	public static void main(String[] args) {
+
+		if (a==1){
+			System.out.println("Hello, Serge!");
+		}
+			else 
+		{
+			System.out.println("Nothing");
+		}
+		
+
+		//System.out.println("Hello again");
+	}
+}
